7bfad2ea47855625d48ae042255c475c43a4a0a5
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-12 19:01:28 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -16,6 +16,7 @@ def third_challenge
         },
       :hero_friends => []
    }, 
+   
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet",
@@ -27,7 +28,7 @@ def third_challenge
         },
       :heroine => {
         :name => "Juliet",
-        :age => "15"
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
